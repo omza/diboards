0f36b22dd486169b8e53c26fb25b3b447b32139b
Oliver Meyer zu Altenschildesche <omza@live.de>
2017-07-04 10:01:44 +0200

    nginx ssl initial

--- diboards/config/devcontainer.py
+++ diboards/config/devcontainer.py
@@ -1,5 +1,5 @@
 # Flask settings
-SERVER_NAME = 'devdiboards.com:5000'
+SERVER_NAME = 'devdiboards.com'
 DEBUG = True  # Do not use debug mode in production
 PORT = None
 HOST = None
@@ -13,5 +13,7 @@ RESTPLUS_ERROR_404_HELP = False
 # SQLAlchemy settings
 SQLALCHEMY_DATABASE_URI = 'sqlite:////usr/db/db.diboards'
 SQLALCHEMY_TRACK_MODIFICATIONS = True
+
+#diboards settings
 DIBOARDS_LOGLEVEL_CONSOLE = 10 # Info 
 DIBOARDS_LOGLEVEL_FILE = 10 # debug
